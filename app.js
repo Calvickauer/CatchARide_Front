d4c34ee0d710af995df95994469ae3147c8afb1f
Descotto <davidescotto@hotmail.com>
2022-11-08 14:50:04 -0800

    add images to db, still need associations

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 require('./config/passport')(passport);
 bodyParser = require('body-parser');
+const multer = require('multer');
+// var getRawBody = require('raw-body')
+const contentType = require('content-type');
 
 // App Set up
 const app = express();
@@ -13,10 +16,34 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); // JSON parsing
+app.use(express.json({limit: '5mb'})); // JSON parsing
 app.use(cors()); // allow all CORS requests
 app.use(passport.initialize());
 app.use('/public', express.static('public'));
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+      const fileName = file.originalname.toLowerCase().split(' ').join('-');
+      cb(null, uuidv4() + '-' + fileName)
+  }
+});
+
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+          cb(null, true);
+      } else {
+          cb(null, false);
+          return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+      }
+  }
+});
 
 // Database Set Up
 const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
